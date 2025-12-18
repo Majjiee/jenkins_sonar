19b78e203bcbe1397aab0a22e01dc8f4a56fa7e0
Majjiee <majdoulinesb1@gmail.com>
2025-12-18 11:12:40 +0100

    second commit

--- car/src/main/java/com/example/car/entities/Client.java
+++ car/src/main/java/com/example/car/entities/Client.java
@@ -11,4 +11,24 @@ public class Client {
     private Long id;
     private String nom;
     private Integer age;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+    
+    
 }
